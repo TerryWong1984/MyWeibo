5faa3761f493dbbaeba62bdf553242df72c80aa1
TerryWong1984 <wangshancheng1984@126.com>
2018-03-11 10:34:59 +0800

    初建工程

---
+++ HMWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMWeibo
+//
+//  Created by TerryWong on 2018/3/11.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HMWeibo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HMWeibo
+//
+//  Created by TerryWong on 2018/3/11.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
